eb0a39c8c6136df27b178ed5e5135b96b26db52b
sogabrie <gabrielyan.sos@mail.ru>
2024-05-07 13:48:11 +0400

    KAN #1 config file return option issue

--- src/ParsingConfigFile/ParsingConfigFile.cpp
+++ src/ParsingConfigFile/ParsingConfigFile.cpp
@@ -288,8 +288,8 @@ size_t	ParsingConfigFile::CheckCorecktConfig(std::string config, size_t pos_star
 	}
 	else if (std::string("return") == config)
 	{
-		if (!controlFlag)
-			throw MyException("Error return in server " + this->getErrorline(pos_start));
+		// if (!controlFlag)
+		// 	throw MyException("Error return in server " + this->getErrorline(pos_start));
 		res = checkReturn(pos_start, pos_end, controlFlag);
 		if (SIZE_MAX != res)
 			return (res);
