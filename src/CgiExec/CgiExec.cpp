b5444bee0f2608b214a8ee8b85edc3899058e9ed
DavidOhanyan <davitohanyan630@gmail.com>
2024-05-10 17:32:16 +0400

    ready to test python-cgi

---
+++ src/CgiExec/CgiExec.cpp
@@ -0,0 +1,43 @@
+#include "CgiExec.hpp"
+
+void CgiExec::executeCGI(std::string filePath, std::string fileType)
+{
+
+	(void)fileType;
+
+	int pipefd[2];
+	char buffer[2];
+	std::string res;
+	ssize_t bytesRead;
+
+	char *args[] = {strdup(PYTHONPATH), strdup(filePath.c_str()), NULL};
+
+	if (pipe(pipefd) == -1) 
+	    throw std::runtime_error("pipe");
+
+	int pid = fork();
+
+	if (pid == -1) 
+	    throw std::runtime_error("fork");
+	if (pid == 0)
+	{
+		close(pipefd[0]);
+		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+			throw std::runtime_error("dub2");
+	    close(pipefd[1]);
+	    if (execve(PYTHONPATH, args, NULL) == -1)//env or NULL
+	        throw std::runtime_error("execve");
+	}
+	waitpid(pid, NULL, 0);
+	close(pipefd[1]);
+	while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0)
+	{
+	    buffer[bytesRead] = '\0'; 
+	    res += buffer;
+	}
+	close(pipefd[0]);
+
+	free(args[0]);
+	free(args[1]);
+	std::cout << res <<std::endl;
+}
--- src/CgiExec/CgiExec.hpp
+++ src/CgiExec/CgiExec.hpp
@@ -1,32 +1,16 @@
-#ifndef _CGI_EXEC_
-#define _CGI_EXEC_
+#ifndef _CGI_EXEC_HPP_
+#define _CGI_EXEC_HPP_
 
 #include <iostream>
 #include <string>
 #include <unistd.h>
 
+#define PYTHONPATH "./CGI_interpritatrs/python-cgi-mac"
 
 class CgiExec
 {
 public:
-	static void executeCGI(std::string filePath, std::string fileType)
-	{
-		int pid = fork();
-		if (pid == 0)
-		{
-			
-			const char *pythonPath = "/usr/bin/python3";  
-    		const char *scriptPath = "example.py";      
-
-    		char *const args[] = {pythonPath, scriptPath, NULL};
-    		char *const env[] = {NULL};
-
-    		execve(pythonPath, args, env);
-
-    		// Код после execve выполнится только в случае ошибки
-    		perror("execve");
-		}
-	}
+	static void executeCGI(std::string filePath, std::string fileType);
 };
 
 #endif
